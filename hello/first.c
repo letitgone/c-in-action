64cdd4447b1e743c3b68e4c57061d73b760d2f66
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2021-05-20 00:02:16 +0800

    Gavin:Add first code

---
+++ hello/first.c
@@ -0,0 +1,14 @@
+/**
+ * @Author ZhangGJ
+ * @Date 2021/05/19 23:55
+ */
+#include <stdio.h>
+
+int main(void) {
+    int num;
+    num = 1;
+    printf("I am a simple ");
+    printf("computer \n");
+    printf("My favorite number is %d because it is first.\n", num);
+    return 0;
+}
