b06681a1d615a95496643efe30edea9d08963aeb
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2021-06-30 06:12:19 +0800

    Gavin:Input

---
+++ input/input.c
@@ -0,0 +1,18 @@
+/**
+ * @Author ZhangGJ
+ * @Date 2021/06/29 23:06
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int age;
+    float assets;
+    char pet[30];
+
+    printf("Enter your age, assets, and favorite pet.\n");
+    scanf("%d %f", &age, &assets);
+    scanf("%s", pet);
+    printf("%d $%.2f %s\n", age, assets, pet);
+    return 0;
+}
